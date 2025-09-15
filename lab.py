c016804c4083058f2c0409b3d87554d7e3a0ebfa
Vladimir Shishmakov <vova2005761@gmail.com>
2025-09-15 16:57:52 +0300

    sum without overflow

--- lab.py
+++ lab.py
@@ -8,6 +8,33 @@ def from_int(num, M, N):
         result.append(new_element)
         num //= M
     result.reverse()
-    return result
+    return normalize(result, M, N)
 
-print(from_int(35, M, N))
+def normalize(num, M, N):
+    while num[0] == 0:
+        num = num[1:]
+    return num
+
+def sum(first_value, second_value, M, N):
+    first_len = len(first_value)
+    second_len = len(second_value)
+    max_len = max(first_len, second_len) + 1
+    first_value = (max_len - first_len)*[0] + first_value
+    second_value = (max_len - second_len)*[0] + second_value
+    first_value.reverse()
+    second_value.reverse()
+    result = []
+    carry_over = 0
+    for i in range(max_len):
+        res = first_value[i] + second_value[i] + carry_over
+        result.append(res % M)
+        if res >= M:
+            carry_over = 1
+        else:
+            carry_over = 0
+    result.reverse()
+    return normalize(result, M, N)
+
+a = from_int(0b11, M, N)
+b = from_int(0b1, M, N)
+print(sum(a, b, M, N))
