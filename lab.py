2c937d9c79f841b19f24e15f3124f2f058f848d0
Vladimir Shishmakov <vova2005761@gmail.com>
2025-09-28 18:45:57 +0300

    fix div (added additional function mul_small)

--- lab.py
+++ lab.py
@@ -1,132 +1,137 @@
 M = 2**15
 N = 100
 
-def from_int(num, M, N):
-    result = []
-    for i in range(N):
-        new_element = num % M
-        result.append(new_element)
-        num //= M
-    result.reverse()
-    return normalize(result, M, N)
-
-def compare(first_value, second_value):
-    if len(first_value) > len(second_value):
-        return 1
-    elif len(second_value) > len(first_value):
-        return -1
-    else:
-        for i in range(len(first_value)):
-            if first_value[i] > second_value[i]:
-                return 1
-            if second_value[i] > first_value[i]:
-                return -1
-    return 0
-
-def normalize(num, M, N):
-    while num[0] == 0 and len(num) > 1:
+def normalize(num, M=None, N=None):
+    while len(num) > 1 and num[0] == 0:
         num = num[1:]
     return num
 
+def from_int(num, M=2**15, N=100):
+    if num == 0:
+        return [0]
+    digits = []
+    while num > 0:
+        digits.append(num % M)
+        num //= M
+    digits.reverse()
+    return normalize(digits)
+
+def compare(first_value, second_value):
+    a = normalize(first_value[:])
+    b = normalize(second_value[:])
+    if len(a) > len(b):
+        return 1
+    if len(a) < len(b):
+        return -1
+    for x, y in zip(a, b):
+        if x > y:
+            return 1
+        if x < y:
+            return -1
+    return 0
+
 def sum(first_value, second_value, M, N):
-    first_len = len(first_value)
-    second_len = len(second_value)
-    max_len = max(first_len, second_len) + 1
-    first_value = (max_len - first_len)*[0] + first_value
-    second_value = (max_len - second_len)*[0] + second_value
-    first_value.reverse()
-    second_value.reverse()
-    result = []
-    carry_over = 0
-    for i in range(max_len):
-        res = first_value[i] + second_value[i] + carry_over
-        result.append(res % M)
-        if res >= M:
-            carry_over = 1
-        else:
-            carry_over = 0
-    if len(result) > N:
-        result = result[:N]
-    result.reverse()
-    return normalize(result, M, N)
+    a = first_value[:]
+    b = second_value[:]
+    if len(a) < len(b):
+        a = [0] * (len(b) - len(a)) + a
+    if len(b) < len(a):
+        b = [0] * (len(a) - len(b)) + b
+    carry = 0
+    res = [0] * (len(a) + 1)
+    for i in range(len(a) - 1, -1, -1):
+        s = a[i] + b[i] + carry
+        res[i + 1] = s % M
+        carry = s // M
+    res[0] = carry
+    return normalize(res)
 
 def sub(first_value, second_value, M, N):
     if compare(first_value, second_value) == 0:
         return [0]
-    elif compare(first_value, second_value) == 1:
-        first_len = len(first_value)
-        second_len = len(second_value)
-        if first_len > second_len:
-            second_value = (first_len - second_len)*[0] + second_value
-        carry_over = 0
-        first_value.reverse()
-        second_value.reverse()
-        result = []
-        for i in range(first_len):
-            if first_value[i] >= second_value[i] + carry_over:
-                res = first_value[i] - second_value[i] - carry_over
-                carry_over = 0
+    if compare(first_value, second_value) == 1:
+        a = first_value[:]
+        b = second_value[:]
+        if len(b) < len(a):
+            b = [0] * (len(a) - len(b)) + b
+        res = [0] * len(a)
+        borrow = 0
+        for i in range(len(a) - 1, -1, -1):
+            val = a[i] - b[i] - borrow
+            if val < 0:
+                val += M
+                borrow = 1
             else:
-                res = M - abs(first_value[i] - second_value[i] - carry_over)
-                carry_over = 1
-            result.append(res)
-        result.reverse()
-        return normalize(result, M, N)
+                borrow = 0
+            res[i] = val
+        return normalize(res)
     else:
-        new_second_value = sub(sub(second_value, first_value, M, N), from_int(1, M, N), M, N)
-        new_first_value = [M-1] * N
-        return sub(new_first_value, new_second_value, M, N)
+        temp = sub(second_value, first_value, M, N)
+        temp_minus_one = sub(temp, from_int(1, M, N), M, N)
+        new_first = [M - 1] * N
+        return sub(new_first, temp_minus_one, M, N)
+
+def mul_small(a, k, M, N=None):
+    if k == 0 or a == [0]:
+        return [0]
+    a = a[:]
+    res = [0] * (len(a) + 1)
+    carry = 0
+    for i in range(len(a) - 1, -1, -1):
+        prod = a[i] * k + carry
+        res[i + 1] = prod % M
+        carry = prod // M
+    res[0] = carry
+    return normalize(res)
 
 def times(first_value, second_value, M, N):
-    result = [0]
-    first_value.reverse()
-    second_value.reverse()
+    a = normalize(first_value[:])
+    b = normalize(second_value[:])
+    if a == [0] or b == [0]:
+        return [0]
+    res = [0] * (len(a) + len(b))
+    # schoolbook multiplication, без мутаций
+    for i in range(len(a) - 1, -1, -1):
+        carry = 0
+        for j in range(len(b) - 1, -1, -1):
+            idx = i + j + 1
+            total = res[idx] + a[i] * b[j] + carry
+            res[idx] = total % M
+            carry = total // M
+        res[i] += carry
+    return normalize(res)
 
-    for i in range(len(second_value)):
-        if second_value[i] == 0:
-            continue
-        
-        carry_over = 0
-        res = []
-        for j in range(len(first_value)):
-            t = first_value[j] * second_value[i] + carry_over
-            res.append(t % M)
-            carry_over = t // M
-        if carry_over > 0:
-            res.append(carry_over)
-        res.reverse()
-        res = res + [0] * i
-        
-        result = sum(result, res, M, N)
-    return normalize(result, M, N)
-
-def div(first_value, second_value, M, N): 
+def div(first_value, second_value, M, N):
     if compare(second_value, [0]) == 0:
         raise ZeroDivisionError("division by zero")
-    
-    if compare(first_value, second_value) == -1:
+    a = normalize(first_value[:])
+    b = normalize(second_value[:])
+    if compare(a, b) == -1:
         return [0]
-    
-    result = [0]
+    quotient = []
     remainder = [0]
+    for digit in a:
+        remainder = [digit] if remainder == [0] else (remainder + [digit])
+        remainder = normalize(remainder)
+        
+        lo, hi = 0, M - 1
+        q = 0
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            prod = mul_small(b, mid, M, N)
+            if compare(prod, remainder) <= 0:
+                q = mid
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        quotient.append(q)
+        if q:
+            remainder = sub(remainder, mul_small(b, q, M, N), M, N)
+    return normalize(quotient)
 
-    for i in range(len(first_value)):
-        
-        remainder = remainder + [first_value[i]]
-        remainder = normalize(remainder, M, N)
-        
-        count = 0
-        
-        while compare(remainder, second_value) >= 0:
-            remainder = sub(remainder, second_value, M, N)
-            count += 1
-        
-        result = result + [count]
-        result = normalize(result, M, N)
-    
-    return normalize(result, M, N)
 
 
-a = from_int(0b110, M, N)
-b = from_int(0b10, M, N)
+
+a = from_int(10000000000, M, N)
+b = from_int(1000000000, M, N)
 print(sum(a, b, M, N), sub(a, b, M, N), times(a, b, M, N), div(a, b, M, N))
